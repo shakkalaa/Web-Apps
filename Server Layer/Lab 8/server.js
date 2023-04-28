7e7ef4abccac97e152ec552695e8969608f8eb14
shakkalaa <85145974+shakkalaa@users.noreply.github.com>
2023-04-28 11:12:47 -0500

    commit

--- Server Layer/Lab 8/server.js
+++ Server Layer/Lab 8/server.js
@@ -1,17 +1,76 @@
-const http = require ( 'http' );
-const hostname = '127.0.0.1' ; //or 'localhost'
-const port = 3000 ; 
+const http = require( 'http' );
+const hostname = '127.0.0.1'; //or 'localhost'
+const port = 3000; 
 
-const server = http . createServer ( httpHandler )
-server . listen ( port , hostname , serverMessage ) 
+const url = require( 'url' );
+const path = require( 'path' ); 
+
+const fileSystem = require ( 'fs' ); 
 
 
-function httpHandler ( request , response ){
-    response . statusCode = 200 ; //Status Code: 200 - Success
-    response . setHeader ( 'Content-Type' , 'text/plain' ); //Response Content Type: text
-    response . end ( 'Hello World \n ' ); //End Response, pass data
+const server = http.createServer( httpHandler )
+server.listen( port , hostname , serverMessage ) 
+
+
+function httpHandler( request , response ){
+    let urlObject = url.parse ( request . url );
+    let uri = urlObject.pathname ;
+    let fileName = path.join ( process . cwd (), uri );
+    console.log ( 'Loading ' + uri ); 
+
+    let stats ;
+    try {
+        stats = fileSystem.lstatSync ( fileName );
+        if( stats.isFile () ){
+            serveFile( response , fileName )
+        }
+        else if( stats . isDirectory ()){
+            serveIndex( response );
+        } 
+        else{
+            serveError ();
+        } 
+           
+    } catch( e ){
+        fileNotFound( response )
+    } 
 } 
 
-function serverMessage (){
-    console . log ( `Server running at http:// ${ hostname } : ${ port } /` )
+function fileNotFound( response ){
+    response.writeHead( 404 , { 'Content-type' : 'text/plain' });
+    response.write( '404 Not Found \n ' );
+    response.end();
+    return;
 } 
+
+function serverMessage(){
+    console.log( `Server running at http:// ${ hostname } : ${ port } /` )
+} 
+
+const mimeTypes = {
+    ".html" : "text/html" ,
+    ".jpeg" : "image/jpeg" ,
+    ".jpg" : "image/jpg" ,
+    ".png" : "image/png" ,
+    ".js" : "text/javascript" ,
+    ".css" : "text/css"
+}; 
+
+function serveFile( response , fileName ){
+    let fileExtension = path.extname( fileName ); //get file extension to get MIME type
+    let mimeType = mimeTypes[ fileExtension ];
+    response.writeHead( 200 ,{ 'Content-type' : mimeType });
+    let fileStream = fileSystem.createReadStream( fileName );
+    fileStream.pipe( response ); 
+}
+
+function serveIndex( response ){
+    response.writeHead( 302 , { 'Location' : 'index.html' })
+    response.end();
+} 
+
+function serveError ( response ){
+    response . writeHead ( 500 , { 'Content-type' : 'text/plain' });
+    response . write ( '500 Internal Error \n ' );
+    response . end ();
+}
